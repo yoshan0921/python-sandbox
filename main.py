99cbfe9e1b6c56dbfa417e6c8e42d5767ede4778
YOSUKEHANAOKA <yhana0921@gmail.com>
2023-05-02 17:24:04 +0900

    Added binary_search

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+def binary_search(data: list, value: int):
+    left = 0
+    right = len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == value:
+            return mid
+        elif data[mid] < value:
+            left = mid + 1
+        else:
+            right = mid -1
+    return -1
+
+if __name__ == "__main__":
+    data = [10, 20, 30, 40, 50, 60, 70, 80]
+    result = binary_search(data, 40)
+    print(result)
+
