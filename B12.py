78c47a949ebc22050f401bafa0253ab3679fd905
YOSUKEHANAOKA <yhana0921@gmail.com>
2023-05-07 16:38:05 +0900

    practice

---
+++ B12.py
@@ -0,0 +1,30 @@
+import io
+import sys
+
+_INPUT = """\
+10
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+
+def f(x):
+    return x*x*x + x
+
+
+N = int(input())
+
+# 二分探索
+Left = 0.0
+Right = 100.0
+for i in range(20):
+    Mid = (Left + Right) / 2
+    val = f(Mid)
+
+    # 探索範囲を絞る
+    if val > N:
+        Right = Mid  # 左半分に絞られる
+    else:
+        Left = Mid  # 右半分に絞られる
+
+# 出力
+print(Mid)
