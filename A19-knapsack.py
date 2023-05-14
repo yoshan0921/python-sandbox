f7c9dce34e44dab3684eb30efcef5abf9b4c6e02
YOSUKEHANAOKA <yhana0921@gmail.com>
2023-05-15 08:29:25 +0900

    updated

--- A19-knapsack.py
+++ A19-knapsack.py
@@ -1,3 +1,16 @@
+import io
+import sys
+
+_INPUT = """\
+4 7
+3 13
+3 17
+5 29
+1 10
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+
 # 入力
 N, W = map(int, input().split())
 w = [None] * (N + 1)
@@ -5,8 +18,12 @@ v = [None] * (N + 1)
 for i in range(1, N+1):
     w[i], v[i] = map(int, input().split())
 
+# test = [[-10 ** 15] * (W + 1)]
+# print(test)
+
 # 動的計画法
-dp = [[-10 ** 15] * (W + 1) for i in range(N + 1)]
+dp = [[0] * (W + 1) for i in range(N + 1)]
+# print(dp)
 dp[0][0] = 0
 for i in range(1, N+1):
     for j in range(0, W+1):
@@ -16,4 +33,5 @@ for i in range(1, N+1):
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]]+v[i])
 
 # 出力
-print(max(dp[N]))
+print(dp)
+# print(max(dp[N]))
